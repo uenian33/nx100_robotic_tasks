718115ca91570719337217965e8c4445566eb198
uenian33 <wenyany94@gmail.com>
2021-06-27 11:55:33 +0300

    cleaned the remote control test file

--- run_grasp_generator.py
+++ run_grasp_generator.py
@@ -2,7 +2,18 @@ from libs.grasp_estimation.inference.grasp_generator import GraspGenerator
 from libs.camera.custom_camera import CustomCamera
 from libs.camera.camera_data import CameraData
 
-       
+
+from nx100_remote_control.module import Commands, Utils
+from nx100_remote_control.objects import MoveL
+
+def callback_success():
+    print('MoveL position has been reached')
+
+def callback_failed():
+    print('MoveL error or position not reached on given timeout')
+
+
+
 
 if __name__ == '__main__':
 	# init camera setups
@@ -21,18 +32,23 @@ if __name__ == '__main__':
     generator.camera.stream()
     target_robot_pose = generator.generate()
 
-    # execut the target reaching
-	Commands.write_linear_move(MoveL.MoveL(
-	        0, 45, 0,
-	        target_robot_pose[0],
-	        target_robot_pose[1],
-	        target_robot_pose[2],
-	        target_robot_pose[3],
-	        target_robot_pose[4],
-	        target_robot_pose[5],
-	        Utils.binary_to_decimal(0x00000001)
-	    ))
+    move_l = MoveL.MoveL(
+	    MoveL.MoveL.motion_speed_selection_posture_speed,
+	    5,
+	    MoveL.MoveL.coordinate_specification_base_coordinate,
+        target_robot_pose[0],
+        target_robot_pose[1],
+        target_robot_pose[2],
+        target_robot_pose[3],
+        target_robot_pose[4],
+        target_robot_pose[5],
+	    Utils.binary_to_decimal(0x00000001),
+	    0, 0, 0, 0, 0, 0, 0
+	)
+	    
+	Commands.robot_in_target_point_callback(
+	    move_l=move_l, timeout=10, _callback_success=callback_success, _callback_failed=callback_failed
+	)
 
-	time.sleep(10)
 	Gripper.write_gripper_close()
 
