e172f470bcfce7115b355e4ee82101b05a6ca781
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-07-03 23:24:12 +0300

    Reformat file

--- libs/grasp_estimation/inference/grasp_generator.py
+++ libs/grasp_estimation/inference/grasp_generator.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-#from libs.camera.custom_camera import CustomCamera
-#from libs.camera.camera_data import CameraData
+# from libs.camera.custom_camera import CustomCamera
+# from libs.camera.camera_data import CameraData
 
 from libs.grasp_estimation.hardware.device import get_device
 from libs.grasp_estimation.inference.post_process import post_process_output
@@ -14,31 +14,29 @@ from libs.grasp_estimation.utils.dataset_processing.grasp import detect_grasps
 from libs.grasp_estimation.utils.visualisation.plot import plot_grasp
 
 # libs from motoman remote control https://github.com/norkator/nx100-remote-control/blob/b7a4392f0e182670986ca9488ccd58a3d09ae8b1/XboxController.py#L41
-from libs.nx100_remote.module import  Commands, Utils, Gripper
-from libs.nx100_remote.objects import Time, MoveL
+from nx100_remote_control.module import Commands, Utils, Gripper
 
 import cv2
 
 
 class GraspGenerator:
-    def __init__(self, camera, cam_data,  saved_model_path, cam_id, visualize=False):
+    def __init__(self, camera, cam_data, saved_model_path, cam_id, visualize=False):
         self.saved_model_path = saved_model_path
-        self.camera = camera #CustomCamera()
+        self.camera = camera  # CustomCamera()
 
         self.saved_model_path = saved_model_path
         self.model = None
         self.device = None
 
-        self.cam_data = cam_data#CameraData(include_depth=True, include_rgb=True)
+        self.cam_data = cam_data  # CameraData(include_depth=True, include_rgb=True)
 
         # Connect to camera
-        #self.camera.connect()
+        # self.camera.connect()
 
         # Load camera pose and depth scale (from running calibration)
-        #self.cam_pose = np.loadtxt('saved_data/camera_pose.txt', delimiter=' ')
-        #self.cam_depth_scale = np.loadtxt('saved_data/camera_depth_scale.txt', delimiter=' ')
-        #self. = 
-
+        # self.cam_pose = np.loadtxt('saved_data/camera_pose.txt', delimiter=' ')
+        # self.cam_depth_scale = np.loadtxt('saved_data/camera_depth_scale.txt', delimiter=' ')
+        # self. =
 
         homedir = os.path.join(os.path.expanduser('~'), "grasp-comms")
         self.grasp_request = os.path.join(homedir, "grasp_request.npy")
@@ -54,8 +52,6 @@ class GraspGenerator:
         self.WAIT_FOR = 0.25  # seconds
         self.COORDINATE_SYSTEM = 0
 
-    
-
     def load_model(self):
         print('Loading model... ')
         self.model = torch.load(self.saved_model_path)
@@ -65,11 +61,11 @@ class GraspGenerator:
     def generate(self):
         # Get RGB-D image from camera
         image_bundle = self.camera.get_image_bundle()
-        rgb = image_bundle[0]#['rgb']
+        rgb = image_bundle[0]  # ['rgb']
         rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
-        depth = image_bundle[1]#['aligned_depth']
-        #depth = cv2.cvtColor(depth,cv2.COLOR_GRAY2BGR)
-        #print(rgb.shape, depth.shape)
+        depth = image_bundle[1]  # ['aligned_depth']
+        # depth = cv2.cvtColor(depth,cv2.COLOR_GRAY2BGR)
+        # print(rgb.shape, depth.shape)
         x, depth_img, rgb_img = self.cam_data.get_data(rgb=rgb, depth=depth)
 
         # Predict the grasp pose using the saved model
@@ -83,9 +79,9 @@ class GraspGenerator:
 
         if self.fig:
             plot_grasp(fig=self.fig, rgb_img=self.cam_data.get_rgb(rgb, False), grasps=grasps, save=False)
-        #print(grasps)
+        # print(grasps)
 
-        #"""
+        # """
         # Get grasp position from model output
 
         # the image resize scale
@@ -94,24 +90,23 @@ class GraspGenerator:
         xy_scale = origin_w / curr_im_w
 
         # the xyz offset between camera and gripper
-        z_offset = 1*130.7
+        z_offset = 1 * 130.7
         x_offset = -55.87
         y_offset = 50.77
 
-
-        print( depth[grasps[0].center[0], grasps[0].center[1]])
-        pos_z = self.camera.depth_scale * depth[grasps[0].center[0], grasps[0].center[1]] - 0.04 #self.camera.left_intrinsics[0][0] / depth[grasps[0].center[0], grasps[0].center[1]] - 0.04
+        print(depth[grasps[0].center[0], grasps[0].center[1]])
+        pos_z = self.camera.depth_scale * depth[grasps[0].center[0], grasps[0].center[
+            1]] - 0.04  # self.camera.left_intrinsics[0][0] / depth[grasps[0].center[0], grasps[0].center[1]] - 0.04
         pos_x = np.multiply(grasps[0].center[1] * xy_scale - self.camera.left_intrinsics[0][2],
                             pos_z / self.camera.left_intrinsics[0][0])
         pos_y = np.multiply(grasps[0].center[0] * xy_scale - self.camera.left_intrinsics[1][2],
                             pos_z / self.camera.left_intrinsics[1][1])
-        
-        #pos_z = depth[grasps[0].center[0], grasps[0].center[1]] * self.cam_depth_scale - 0.04
-        #pos_x = np.multiply(grasps[0].center[1] + self.cam_data.top_left[1] - self.camera.intrinsics.ppx,
-        #                    pos_z / self.camera.intrinsics.fx)
-        #pos_y = np.multiply(grasps[0].center[0] + self.cam_data.top_left[0] - self.camera.intrinsics.ppy,
-        #                    pos_z / self.camera.intrinsics.fy)
 
+        # pos_z = depth[grasps[0].center[0], grasps[0].center[1]] * self.cam_depth_scale - 0.04
+        # pos_x = np.multiply(grasps[0].center[1] + self.cam_data.top_left[1] - self.camera.intrinsics.ppx,
+        #                    pos_z / self.camera.intrinsics.fx)
+        # pos_y = np.multiply(grasps[0].center[0] + self.cam_data.top_left[0] - self.camera.intrinsics.ppy,
+        #                    pos_z / self.camera.intrinsics.fy)
 
         if pos_z == 0:
             return
@@ -120,22 +115,20 @@ class GraspGenerator:
         target.shape = (3, 1)
         print('target: ', target, grasps[0].angle)
         c_pose = self.get_end_effector_position()
-        print(np.array([-pos_x*10 + x_offset,
-                -pos_y*10 + y_offset,
-                -pos_z*10,
-                -pos_z*0 + z_offset]))
+        print(np.array([-pos_x * 10 + x_offset,
+                        -pos_y * 10 + y_offset,
+                        -pos_z * 10,
+                        -pos_z * 0 + z_offset]))
 
         angle = grasps[0].angle / np.pi * 180
 
-        target_robot_pose = [c_pose.x+pos_x*10 + x_offset,
-                            c_pose.y-pos_y*10 + y_offset,
-                            c_pose.z-pos_z*10 + z_offset,
-                            c_pose.tx,
-                            c_pose.ty,
-                            c_pose.tz + angle]
-        
-       
- 
+        target_robot_pose = [c_pose.x + pos_x * 10 + x_offset,
+                             c_pose.y - pos_y * 10 + y_offset,
+                             c_pose.z - pos_z * 10 + z_offset,
+                             c_pose.tx,
+                             c_pose.ty,
+                             c_pose.tz + angle]
+
         """
         c_pose = self.get_end_effector_position()
         
@@ -151,10 +144,6 @@ class GraspGenerator:
                 Utils.binary_to_decimal(0x00000001)
             ))
         """
-        
-
-        
-
 
         """
         # Convert camera to robot coordinates
@@ -174,14 +163,14 @@ class GraspGenerator:
 
         np.save(self.grasp_pose, grasp_pose)
         """
-        #if self.fig:
+        # if self.fig:
         #    plot_grasp(fig=self.fig, rgb_img=self.cam_data.get_rgb(rgb, False), grasps=grasps, save=False)
 
-        return target_robot_pose#np.append(target_position, target_angle)
+        return target_robot_pose  # np.append(target_position, target_angle)
 
     def run(self):
-        #Gripper.write_gripper_open()
-        #while True:
+        # Gripper.write_gripper_open()
+        # while True:
         grasp_pose = self.generate()
         """
         Commands.write_linear_move(MoveL.MoveL(
@@ -211,6 +200,6 @@ class GraspGenerator:
             else:
                 time.sleep(0.1)
         """
+
     def get_end_effector_position(self):
         return Commands.read_current_specified_coordinate_system_position('0', '0')
-
